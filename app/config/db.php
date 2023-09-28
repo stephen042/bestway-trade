abb4c26f8f0cb1813732ab0ee7b020513ff8eaee
Ani stephen Makuochukwu <84373037+stephen042@users.noreply.github.com>
2023-09-28 18:05:33 +0100

    Update db.php

--- app/config/db.php
+++ app/config/db.php
@@ -1,9 +1,9 @@
 <?php
 
 @define('DBSERVER', 'localhost');
-@define('DBUSERNAME', 'root');
-@define('DBPASSWORD', '');
-@define('DBNAME', 'astromine');
+@define('DBUSERNAME', 'u119630305_bestwaytrade');
+@define('DBPASSWORD', 'Secret');
+@define('DBNAME', 'u119630305_bestwaytrade');
 @define('DBPORT', '3308');
 
  
