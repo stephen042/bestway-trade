4b65b04527133b31dc2c867e11f4e53cd84afe55
stepheno42 <84373037+stephen042@users.noreply.github.com>
2023-09-29 12:00:43 +0100

    logo

--- app/user/header.php
+++ app/user/header.php
@@ -136,7 +136,7 @@ if (isset($_GET['switch'])) {
         <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="navbar-brand-wrapper d-flex justify-content-center">
                 <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-                    <a class="navbar-brand brand-logo" href="index.php"><img src="../img/logo.png" alt="logo" /></a>
+                    <a class="navbar-brand brand-logo" href="index.php"><h1 style="font-weight: bold;color:grey;" class="btn btn-light">BestWay Trade</h1></a>
                     <a class="navbar-brand brand-logo-mini" href="index.php"><img src="../assets/img/favicon.png" alt="logo" /></a>
                     <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
                         <span class="fa fa-sort"></span>
